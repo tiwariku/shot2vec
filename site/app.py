773c2a63959401ce0b4419343d9cfa112b644672
Kunal Tiwari <kunalltiwari@gmail.com>
2019-07-18 16:23:07 -0400

    callback with button to get game json

--- site/app.py
+++ site/app.py
@@ -1,4 +1,5 @@
 #! /usr/local/bin/python3 
+import in_out as io
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -8,30 +9,61 @@ import base64
 from numpy.random import randint 
 from requests_futures.sessions import FuturesSession
 import functions as fn
+import model_fns as mf
 session = FuturesSession(max_workers=2)
 
 
 ##external asses
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-##callbacks
+## the model and other static global variables
+id_2_event = fn._temp_load_coarse_variable('id_2_event')
+event_2_id = fn._temp_load_coarse_variable('event_2_id')
+vocabulary = fn._temp_load_coarse_variable('vocabulary')
+hidden_size = 20
+model_predicting = mf.make_prediction_model_file('./assets/model-30.hdf5',
+                                       vocabulary,
+                                       hidden_size=20)
 
 
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 ## html elements
 title = html.H1(children='Shot2Vec')
-hockey_rink = html.Div([html.H2('Recent Plays'),
+hockey_rink = html.Div([html.H2(id='rink_div', children='Recent Plays'),
                         dcc.Graph(id='rink_plot',
                                   figure=fn.make_rink_fig(),
                         #style={'height':255, 'width':600},
                        )])
-predictions = fn.make_probs_html(fn.get_random_game())
 
-layout_kids = [title, hockey_rink, predictions]
+game_data = html.Div(id='game_json',
+                     style={'display': 'none'})
+
+get_game_button = html.Button(id='get game',
+                              children='Get game data',
+                              n_clicks=0)
+
+step_forward_buttom = html.Button(id='step forward',
+                                  children='Step forward',
+                                  n_clicks=1)
+
+buttons = html.Div(children=[get_game_button, step_forward_buttom],
+                   style={'columnCount':2}
+                  )
+
+layout_kids = [title, buttons, hockey_rink, game_data]
 layout = html.Div(layout_kids)
+app.layout = layout
+# callbacks
+@app.callback(
+    Output(component_id='game_json', component_property='children'),
+    [Input('get game', 'n_clicks')],
+    #state=[State(component_id='game_json', component_property='style')]
+    )
+def update_game_json(n_clicks):
+    return io.get_game_json()#html.Div('BYE!{}'.format(n_clicks))
+
 
 
 if __name__=='__main__':
-    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
     #print(fn.get_probs(fn.get_random_game()[:40]))
-    app.layout = layout 
     app.run_server(debug=True)
