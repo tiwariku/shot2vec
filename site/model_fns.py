395957a11641f11651adc3555ebabfc99ee66503
Kunal Tiwari <kunalltiwari@gmail.com>
2019-07-22 15:59:16 -0400

    reverted KerasBatchGenerator to the flatten and continuous stream

--- site/model_fns.py
+++ site/model_fns.py
@@ -11,8 +11,42 @@ from tensorflow.keras import layers
 import numpy as np
 import random
 
-
 class KerasBatchGenerator(object):
+    '''
+    generates batches for Keras to train neural networks
+    should I grab the batches randomly?
+    '''
+    def __init__(self, data, num_steps, batch_size, vocabulary, skip_step=5):
+        self.data = [play for game in data
+                          for play in game]
+        self.num_steps = num_steps
+        self.batch_size = batch_size
+        self.vocabulary = vocabulary
+        self.current_idx = 0
+        self.skip_step = skip_step
+        
+    def generate(self):
+        while True:#never terminate
+            #input is just the number of steps in each in, and the batch size
+            x = np.zeros((self.batch_size, 
+                          self.num_steps))
+            #output will be one-hots of dimension vocabulary
+            y = np.zeros((self.batch_size, 
+                          self.num_steps, 
+                          self.vocabulary))
+            for i in range(self.batch_size):
+                #if I would run over the edge, reset idx
+                if self.current_idx + self.num_steps >= len(self.data):
+                    self.current_idx = 0
+                x[i,:] = self.data[self.current_idx:self.current_idx + self.num_steps]
+                temp_y = self.data[self.current_idx + 1:self.current_idx + self.num_steps+1]
+                #make the one-hots for the y training data
+                y[i,:,:] = to_categorical(temp_y, 
+                                          num_classes=self.vocabulary)
+                self.current_idx += self.skip_step
+            yield x, y
+
+class KerasBatchGenerator_rand(object):
     '''
     generates batches for Keras to train neural networks
     should I grab the batches randomly?
