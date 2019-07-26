da55afadb0b8720bc0cfc06af20ea226de4348a0
Kunal Tiwari <kunalltiwari@gmail.com>
2019-07-26 17:01:48 -0400

    Rewriting website to handle new model and cache selected game. Nothing works right now. This is a checkpoint in case things get worse.

--- site/app.py
+++ site/app.py
@@ -1,107 +1,131 @@
-#! /usr/local/bin/python3 
-import in_out as io
+'''
+shot2vec webapp
+@tiwariku
+'''
+#import base64
+from ast import literal_eval
+#import uuid
 import dash
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-from dash.dependencies import Input, Output, State
-import base64
-from numpy.random import randint 
-from requests_futures.sessions import FuturesSession
+#import plotly.graph_objs as go
+#from numpy.random import randint
+#from flask_caching import Cache
 import functions as fn
 import model_fns as mf
-session = FuturesSession(max_workers=2)
+import in_out as io
+import data_processing as dp
+
+GAME_JSON = None
+
+EXTERNAL_STYLESHEETS = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+CORPUS_FILENAME = '../assets/corpi/full_coords_bin_10'
+ASSET_DIR = './assets/coords_bin10/'
+MODEL_WEIGHTS = ASSET_DIR+'model_weights.hdf5'
+VOCABULARY = dp.unpickle_it(ASSET_DIR+'vocabulary')
+PLAY_TO_ID = dp.unpickle_it(ASSET_DIR+'play_to_id')
+ID_TO_PLAY = dp.unpickle_it(ASSET_DIR+'id_to_play')
+HIDDEN_SIZE = 30
+MODEL_PREDICTING = mf.make_prediction_model_file(MODEL_WEIGHTS,
+                                                 VOCABULARY,
+                                                 hidden_size=HIDDEN_SIZE)
 
 
-##external asses
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+APP = dash.Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS)
 
-## the model and other static global variables
-id_2_event = fn._temp_load_coarse_variable('id_2_event')
-event_2_id = fn._temp_load_coarse_variable('event_2_id')
-vocabulary = fn._temp_load_coarse_variable('vocabulary')
-hidden_size = 20
-model_predicting = mf.make_prediction_model_file('./assets/model-30.hdf5',
-                                       vocabulary,
-                                       hidden_size=20)
-global temp_game_json
-temp_game_json = io.get_game_response().json()
-
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 ## html elements
-game_data = html.Div(id='game_json',
+GAME_DATA = html.Div(id='game_json',
                      style={'display': 'none'},
                      children='')#io.get_game_response().json())#io.get_game_response().content)
 
-title = html.H1(children='Shot2Vec')
+TITLE = html.H1(children='Shot2Vec')
 
-hockey_rink = html.Div([html.H2(id='rink_div', children='Recent Plays'),
+HOCKEY_RINK = html.Div([html.H2(id='rink_div', children='Recent Plays'),
                         dcc.Graph(id='rink_plot',
-                                  figure=fn.make_rink_fig(1, 
-                                      temp_game_json),
-                        #style={'height':255, 'width':600},
-                       )])
+                                  figure=None #fn.make_rink_fig(1, temp_game_json),
+                                 )])
 
-event_list = html.Div(id='recent plays Div', 
-                      children=[html.H4(id='recent plays H4', 
+EVENT_LIST = html.Div(id='recent plays Div',
+                      children=[html.H4(id='recent plays H4',
                                         children='Recent plays'),
-                                html.P(id='recent plays', 
+                                html.P(id='recent plays',
                                        children='No plays yet')])
 
-
-get_game_button = html.Button(id='get game',
+GET_GAME_BUTTON = html.Button(id='get game',
                               children='Get game data',
                               n_clicks=0)
 
-step_forward_buttom = html.Button(id='step forward',
+STEP_FORWARD_BUTTOM = html.Button(id='step forward',
                                   children='Step forward',
                                   n_clicks=1)
 
-buttons = html.Div(children=[get_game_button, step_forward_buttom],
+BUTTONS = html.Div(children=[GET_GAME_BUTTON, STEP_FORWARD_BUTTOM],
                    style={'columnCount':2}
                   )
 
-probs = html.Div(id='probs Div', 
-                 children = [html.H4(id='probs H5', children='Next event:'),
-                             html.P(id='probs', children='RNN OUTPUT')])
+PROBS = html.Div(id='probs Div',
+                 children=[html.H4(id='probs H5', children='Next event:'),
+                           html.P(id='probs', children='RNN OUTPUT')])
 
-
-layout_kids = [title, buttons, hockey_rink, event_list, game_data, probs]
-layout = html.Div(layout_kids)
-app.layout = layout
+LAYOUT_KIDS = [TITLE, BUTTONS, HOCKEY_RINK, EVENT_LIST, GAME_DATA, PROBS]
+LAYOUT = html.Div(LAYOUT_KIDS)
+APP.layout = LAYOUT
 # callbacks
-@app.callback(Output(component_id='game_json', 
+@APP.callback(Output(component_id='game_json',
                      component_property='children'),
               [Input('get game', 'n_clicks')],)
 def update_game_json(n_clicks):
-    return ''#io.get_game_response().content#html.Div('BYE!{}'.format(n_clicks))
+    '''
+    This callback updates the hiddin div 'game_json' to store the
+    string-seriealized json returned from the io.get_game_response.
+    Eventually this funciton should be modified to take as 'state' the year and
+    game number
+    '''
+    if n_clicks > 1:
+        GAME_JSON = io.get_game_response().json()
+    return str('OK...')
 
-@app.callback(Output(component_id='rink_plot', component_property='figure'),
-        [Input('step forward', 'n_clicks')],
-        state=[State(component_id='game_json', component_property='children')])
-def step_forward(n_steps, game_json):
+@APP.callback(Output(component_id='rink_plot', component_property='figure'),
+              [Input('step forward', 'n_clicks')],
+              state=[State(component_id='game_json',
+                           component_property='children')])
+def step_forward(n_steps, game_json_str):
+    '''
+    Thsi callback updates the 'rink'fig' with the game json
+    '''
+    temp_game_json = literal_eval(game_json_str)
     return fn.make_rink_fig(n_steps, temp_game_json)#io.get_game_response().json())
 
-@app.callback(Output(component_id='recent plays', 
-                     component_property='children'), 
+@APP.callback(Output(component_id='recent plays',
+                     component_property='children'),
               [Input('step forward', 'n_clicks')],
-              )
-def update_recent_plays(n_steps):
-    window = 5
+              state=[State(component_id='game_json',
+                           component_property='children')])
+def update_recent_plays(n_steps, game_json_str):
+    '''
+    This callback updates the recent plays list with the most recent plays
+    '''
+    #window = 5
+    temp_game_json = literal_eval(game_json_str)
     plays = fn.get_recent_plays_string(n_steps, temp_game_json)
     return str(plays)#', '.join()
 
 
-@app.callback(Output(component_id='probs', 
-                     component_property='children'), 
+@APP.callback(Output(component_id='probs',
+                     component_property='children'),
               [Input('step forward', 'n_clicks')],
-              )
-def update_probs(n_steps):
-    return str(fn.get_probs(n_steps, temp_game_json, model_predicting))
+              state=[State(component_id='game_json',
+                           component_property='children')])
+def update_probs(n_steps, game_json_str):
+    '''
+    This function calculates the new probabilities
+    '''
+    temp_game_json = literal_eval(game_json_str)
+    return str(fn.get_probs(n_steps, temp_game_json, MODEL_PREDICTING))
 
 
 
 
-if __name__=='__main__':
-    app.run_server(debug=True)
+if __name__ == '__main__':
+    APP.run_server(debug=True)
